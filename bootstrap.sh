33892bbc270fd6c68f35e428e58160ab4956f2f6
Vladimir Budenny <vladimir@budenny.by>
2020-05-11 19:32:39 +0300

    extracted common functionality into "clone_repo" function.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,32 +1,24 @@
 #! /usr/bin/env zsh
 
-# oh-my-zsh
-OH_MY_ZSH_DIR=~/.oh-my-zsh
-if [ -d "${OH_MY_ZSH_DIR}" ]; then
-  echo "oh-my-zsh found"
-else
-  echo "oh-my-zsh not found, install..."
-  git clone https://github.com/robbyrussell/oh-my-zsh.git "${OH_MY_ZSH_DIR}"
-fi
+function clone_repo()
+{
+  url=$1
+  dest=$2
+  if [ -d "${dest}" ]; then
+    echo "${url} already cloned"
+  else
+    git clone "${url}" "${dest}" --depth 1
+  fi
+}
 
+# oh-my-zsh
+clone_repo https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 
 # syntax highlighting
-SYNTAX_PLUGIN_DIR=~/.zsh-syntax-highlighting
-if ! [ -d "${SYNTAX_PLUGIN_DIR}" ]; then
-  echo "syntax highlighting not found, install..."
-  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${SYNTAX_PLUGIN_DIR}" --depth 1 
-fi
-
+clone_repo https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh-syntax-highlighting
 
 # autosuggestions
-AUTOSUGGESTIONS_PLUGIN_DIR=~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-if  [ -d "${AUTOSUGGESTIONS_PLUGIN_DIR}" ]; then
-  echo "autosuggestions found"
-else
-  echo "syntax highlighting not found, install..."
-  git clone https://github.com/zsh-users/zsh-autosuggestions "${AUTOSUGGESTIONS_PLUGIN_DIR}"
-fi
-
+clone_repo https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 
 # setup zsh config
 ln -sf "$(pwd)/zshrc" ~/.zshrc
